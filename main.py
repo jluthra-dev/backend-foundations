c6b318910302a1fab0583a1dd754a330da9b8381
jluthra-dev <jigyasa66@gmail.com>
2025-11-12 18:31:08 +0530

    routes

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from schemas import User, UserCreate
 
 app = FastAPI()
+users = []
 
-@app.get("/")
-def read_root():
-    return {"message" "Hello, FastAPI!"}
+@app.post("/users", response_model=User)
+def create_user(user: UserCreate):
+    new_user = User(id=len(users)+1, **user.dict())
+    users.append(new_user)
+    return new_user
 
-@app.get("/users/{user_id}")
-def read_user(user_id):
-    return {"user_id": user_id, "name": "John Doe"}
+@app.get("/users", response_model=list[User])
+def list_users():
+    return users
+
+@app.get("/users/{user_id}", response_model=User)
+def get_user(user_id: int):
+    for user in users:
+        if user.id == user_id:
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
